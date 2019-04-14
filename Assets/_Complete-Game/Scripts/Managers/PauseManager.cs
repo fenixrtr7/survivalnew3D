4fa65b91a6f8eb051d1f2abf53d0e245a2a2685a
Arturo <ing.arturoescamilla@outlook.com>
2019-04-14 14:37:51 -0500

    14-04-2019

--- Assets/_Complete-Game/Scripts/Managers/PauseManager.cs
+++ Assets/_Complete-Game/Scripts/Managers/PauseManager.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.Audio;
-#if UNITY_EDITOR
+#if UNITY_EDITOR  // No compilara para las otras pltaformas
 using UnityEditor;
 #endif
 
